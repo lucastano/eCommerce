3d7108603ccea7c990de32db35d9cc9ccd257932
lucastano <lucasgabriel.tano@gmail.com>
2023-09-29 20:08:14 -0300

    se agregaron Irepositorios DE USUARIOS

---
+++ eCommerce/eCommerce.LogicaNegocio/IRepositorios/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.LogicaNegocio.IRepositorios
+{
+    public interface IRepositorio<T>
+    {
+        //CRUD
+        void create(T entidad);
+        T? get();
+        void update(T entidad);
+        void delete(T entidad);
+
+
+    }
+}
---
+++ eCommerce/eCommerce.LogicaNegocio/IRepositorios/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using eCommerce.LogicaNegocio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.LogicaNegocio.IRepositorios
+{
+    public interface IUsuarioRepositorio:IRepositorio<Usuario>
+    {
+        IEnumerable<Usuario> GetAllUsuarios();
+        Usuario GetPorId(int id);
+        
+    }
+}
